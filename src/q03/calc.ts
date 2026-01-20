b84c3da55862cec79d04dc1bb5e3cecb1aa1a9df
Jiraphat parteep <68010150@kmitl.ac.th>
2026-01-20 18:33:59 +0700

    14

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -1,17 +1,19 @@
 const op: string = process.argv[2];
 const n1: number = parseFloat(process.argv[3]);
 const n2: number = parseFloat(process.argv[4]);
-
+let x:number = 0
 if(isNaN(n1)||isNaN(n2)||(op ==='div' && n2===0)){
     console.log('Invalid input');
 }else if(op=== 'add'){
-    console.log(n1+n2);
+    x= n1+n2
 }else if(op=== 'sub'){
-    console.log(n1-n2)
+    x=n1-n2
 }else if(op==="mul"){
-    console.log(n1*n2)
+    x=n1*n2
 }else if(op==='div'){
-    console.log(n1/n2)
+    x=n1/n2
 }else{
     console.log('Invalid operator')
 }
+const reult = (x).toString
+console.log(x)
